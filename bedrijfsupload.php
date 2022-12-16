ce1764553920f7d6298a0613a0595a327d1ae7e0
siebewt <siebewitteveen@live.nl>
2022-12-16 13:37:05 +0100

    upload systeem

---
+++ bedrijfsupload.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload</title>
+</head>
+<body>
+<p class="text-preview">Foto die word toegevoegd:</p>
+<div class='image-preview' style='height:200px;'><img id="output" alt='foto' src=""></div>
+<form class="form" action="uploads/adoptieupload.php" method="post" name="cmsform" enctype="multipart/form-data">
+    <h1 class="login-title">Bedrijfs toevoegen</h1>
+    <p>Naam: *</p><input type="text" name="title" id="title" required="required"/>
+    <p>Tekst: *</p><textarea name="body" minlength="100" rows="5" cols="40" required="required" ></textarea>
+    <p>Selecteer logo om te uploaden:<input type="file" name="file" onchange="loadFile(event)" id="file" /></p>
+
+    <a href="index.php" class="link">Terug</a>
+
+    <input type="submit" class="login-button" value="Verzend" name="verzend">
+</form>
+</body>
+</html>
+<script>
+  var loadFile = function(event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };
+</script>
--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+<a href="bedrijfsupload.php" class='table-link'>Nieuwe bedrijf toevoegen</a><br><br>
 <nav>
     <a>test</a>
 </nav>
